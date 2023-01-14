c14da9d3d72876233a5d1fefeac0a424da8a1c8f
adelestine <delestic@oregonstate.edu>
2023-01-14 12:52:32 -0800

    initial C push
    C file created

---
+++ Lab1/Lab1C/Lab1C/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab1C.c
+ *
+ * Created: 1/14/2023 12:51:47 PM
+ * Author : Astrid
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
