672a3203fafe42fcdd8493bbb8bee441585f2f2c
Lucas Plaisted <lucas.plaisted@gmail.com>
2023-03-01 15:13:26 -0800

    partially working

--- Lab6/Lab6Assm/Lab6Assm/Astrid_Delestine_and_Lucas_Plaisted_Lab6_Sourcecode.asm
+++ Lab6/Lab6Assm/Lab6Assm/Astrid_Delestine_and_Lucas_Plaisted_Lab6_Sourcecode.asm
@@ -17,11 +17,7 @@
 								; waitcnt*10ms for delay
 .def	ilcnt = r18				; Inner Loop Counter
 .def	olcnt = r19				; Outer Loop Counter
-.def	speed = r20				; Speed register, max of 15
-.equ	EngEnR = 5				; right Engine Enable Bit
-.equ	EngEnL = 6				; left Engine Enable Bit
-.equ	EngDirR = 4				; right Engine Direction Bit
-.equ	EngDirL = 7				; left Engine Direction Bit
+.def	speeed = r20				; Speed register, max of 15
 
 ;***********************************************************
 ;*	Start of Code Segment
@@ -47,6 +43,7 @@ INIT:
 		out		SPL, mpr		; Load SPL with low byte of RAMEND
 		ldi		mpr, high(RAMEND)
 		out		SPH, mpr		; Load SPH with high byte of RAMEND
+
 		; Configure I/O ports
 			; Initialize Port B for output
 			ldi		mpr, $FF		; Set Port B Data Direction Register
@@ -69,13 +66,13 @@ INIT:
 				; 3:2 - Timer/CounterC compare mode, 00 = disabled
 				; 1:0 - Wave gen mode low half, 01 for 8-bit fast pwm
 			ldi mpr, 0b10_10_00_01	
-			out TCCR1A, mpr
+			sts TCCR1A, mpr
 			; TCCRIB Bits:
 				; 7:5 - not relevant, 0's
 				; 4:3 - Wave gen mode high half, 01 for 8 bit fast pwm
 				; 2:0 - Clock selection, 001 = no prescale
 			ldi mpr, 0b000_01_001
-			out TCCR1B, mpr
+			sts TCCR1B, mpr
 			; Fast PWM, 8-bit mode, no prescaling
 				; In inverting Compare Output mode output is cleared on compare match and set at TOP
 				;
@@ -83,12 +80,12 @@ INIT:
 		; Set TekBot to Move Forward (1<<EngDirR|1<<EngDirL) on Port B
 			ldi mpr, $F0
 			out PINB, mpr
-		; Set initial speed, display on Port B pins 3:0
-			; Counter counts to FF, so speed is a fraction of that.
-			ldi
+		; Set initial speeed, display on Port B pins 3:0
+			ldi speeed, $0F
+			rcall WRITESPD
 		; Enable global interrupts (if any are used)
 			; Not used
-		ldi waitcnt, 10	; Set wait timer to be 100ms
+		ldi waitcnt, 5	; Set wait timer to be 100ms
 
 ;***********************************************************
 ;*	Main Program
@@ -103,16 +100,98 @@ MAIN:
 		sbrs mpr, 4
 		rcall INCSPD
 
-								; if pressed, adjust speed
-								; also, adjust speed indication
-
 		rjmp	MAIN			; return to top of MAIN
 
 ;***********************************************************
 ;*	Functions and Subroutines
 ;***********************************************************
 
-;----------------------------------------------------------------
+;-----------------------------------------------------------
+; Func:	INCSPD
+; Desc:	Increases the "speeed" of the motor by increasing
+;		the width of the pulse. Has built in debouncing.
+;		Prevents going over the max speeed.
+;-----------------------------------------------------------
+INCSPD:
+		; Push to stack
+		push mpr
+
+		inc speeed		; increase the speeed
+		sbrc speeed, 5	; Skip next command if bit 5 is cleared
+						; If bit 5 is set then we are 16+, 15 is max
+		ldi speeed, 15	; If we are over 15, set speeed to 15
+		rcall WRITESPD
+INCHOLD:				; Don't leave until we let go of the button
+		rcall Wait		; Wait 50ms, debouncing
+		in mpr, PIND	; Grab current button value
+		sbrs mpr, 4		; Check if button is still held
+		rjmp INCHOLD	; Stay in loop if held
+		; Pop from stack
+		pop mpr
+		ret						; End a function with RET
+
+;-----------------------------------------------------------
+; Func:	DECSPD
+; Desc:	Decreases the "speeed" of the motor by decreasing
+;		the width of the pulse. Has built in debouncing
+;-----------------------------------------------------------
+DECSPD:
+		; Push to stack
+		push mpr
+		cpi speeed, 0	; If speeed is 0
+		breq DECSKIP	; Don't decrement
+		dec speeed
+		rcall WRITESPD
+		; Pop from stack
+DECHOLD:				; Don't leave until we let go of the button
+		rcall Wait		; Wait 50ms, debouncing
+		in mpr, PIND	; Grab current button value
+		sbrs mpr, 5		; Check if button is still held
+		rjmp DECHOLD	; Stay in loop if held
+DECSKIP:
+		pop mpr
+		ret						; End a function with RET
+
+;-----------------------------------------------------------
+; Func:	MAXSPD
+; Desc:	Increases the "speeed" 
+;-----------------------------------------------------------
+MAXSPD:
+		push mpr
+		ldi speeed, 15
+		rcall WRITESPD
+MAXHOLD:				; Don't leave until we let go of the button
+		rcall Wait		; Wait 100ms, debouncing
+		in mpr, PIND	; Grab current button value
+		sbrs mpr, 7		; Check if button is still held
+		rjmp MAXHOLD	; Stay in loop if held
+		pop mpr
+		ret						; End a function with RET
+
+;-----------------------------------------------------------
+; Func:	WRITESPD
+; Desc:	Sets the timer compares for the current speeed as
+;		well as setting the lower nibble of 
+;-----------------------------------------------------------
+WRITESPD:
+		push mpr
+		ldi mpr, 17		; 255/15 = 17
+		mul speeed, mpr	; speeed*17 = pulse width
+		clr mpr			; set mpr to 0
+		sts OCR1AH, mpr	; write to high byte of both compares
+		mov mpr, R0		; place output into mpr. Max 255 = 1 reg
+		sts OCR1AL, mpr	; write to low byte of both compares
+		clr mpr
+		sts OCR1BH, mpr	; only done because requried
+		mov mpr, R0		; place output into mpr. Max 255 = 1 reg
+		sts OCR1BL, mpr	; write to low byte of both compares
+		ldi mpr, 0b10010000
+		add mpr, speeed
+		out PINB, mpr
+		pop mpr
+		ret
+
+		;----------------------------------------------------------------
 ; Sub:	Wait
 ; Desc:	A wait loop that is 16 + 159975*waitcnt cycles or roughly
 ;		waitcnt*10ms.  Just initialize wait for the specific amount
@@ -138,82 +217,6 @@ ILoop:	dec		ilcnt			; decrement ilcnt
 		pop		ilcnt		; Restore ilcnt register
 		pop		waitcnt		; Restore wait register
 		ret				; Return from subroutine
-
-
-;-----------------------------------------------------------
-; Func:	INCSPD
-; Desc:	Increases the "speed" of the motor by increasing
-;		the width of the pulse. Has built in debouncing.
-;		Prevents going over the max speed.
-;-----------------------------------------------------------
-INCSPD
-		; Push to stack
-		push mpr
-
-		inc speed		; increase the speed
-		sbrc speed, 5	; Skip next command if bit 5 is cleared
-						; If bit 5 is set then we are 16+, 15 is max
-		ldi speed, 15	; If we are over 15, set speed to 15
-		rcall WRITESPD
-INCHOLD:
-		rcall Wait		; Wait 100ms, debouncing
-		in mpr, PIND	; Grab current button value
-		sbrc mpr, 4		; Check if button is still held
-		rjmp INCHOLD
-		; Pop from stack
-		pop mpr
-		ret						; End a function with RET
-
-;-----------------------------------------------------------
-; Func:	DECSPD
-; Desc:	Decreases the "speed" of the motor by decreasing
-;		the width of the pulse. Has built in debouncing
-;-----------------------------------------------------------
-DECSPD:
-		; Push to stack
-		push mpr
-		cpi speed, 0	; If speed is 0
-		breq DECSKIP	; Don't decrement
-		dec speed
-		rcall WRITESPD
-		; Pop from stack
-DECSKIP:
-		pop mpr
-
-		ret						; End a function with RET
-
-;-----------------------------------------------------------
-; Func:	MAXSPD
-; Desc:	Increases the "speed" 
-;-----------------------------------------------------------
-MAXSPD:	; Begin a function with a label
-
-		; If needed, save variables by pushing to the stack
-
-		; Execute the function here
-
-		; Restore any saved variables by popping from stack
-
-		ret						; End a function with RET
-
-;-----------------------------------------------------------
-; Func:	WRITESPD
-; Desc:	Sets the timer compares for the current speed
-;-----------------------------------------------------------
-WRITESPD:
-		push mpr
-		ldi mpr, 17		; 255/15 = 17
-		mul speed, mpr	; speed*17 = pulse width
-		clr mpr			; set mpr to 0
-		out OCR1AH, mpr	; write to high byte of both compares
-		mov mpr, R0		; place output into mpr. Max 255 = 1 reg
-		out OCR1AL, mpr	; write to low byte of both compares
-		clr mpr
-		out OCR1BH, mpr	; only done because requried
-		mov mpr, R0		; place output into mpr. Max 255 = 1 reg
-		out OCR1BL, mpr	; write to low byte of both compares
-		pop mpr
-		ret
 
 ;***********************************************************
 ;*	Stored Program Data
